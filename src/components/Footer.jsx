1add3f60bf945622c2f42f23074899bdac770360
webdevloppement18 <webdevloppement365@gmail.com>
2024-12-06 17:17:40 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className='w-full h-16  bg-[white]'>
-       <p className="text-center text-[red]"> site crée et dévlopé par webdevloppement18 tout droits réservés </p>
+       <p className="text-center text-[red]"> site crée et dévloppé par webdevloppement18 tout droits réservés </p>
     </footer>
   )
 }
